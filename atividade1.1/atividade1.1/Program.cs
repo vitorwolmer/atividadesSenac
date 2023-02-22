7275bf6d6e83360e43dc14b3fe96aec4b7e03a7e
Vitor <wolmervitor@gmail.com>
2023-02-22 13:47:50 -0300

    ativ1.1

---
+++ atividade1.1/atividade1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(">>>Custo com garçons em evento de hotel<<<");
+
+string nomeEvento;
+int qtd, horas;
+double custo;
+
+Console.WriteLine("Digite o nome do evento:");
+nomeEvento = Console.ReadLine();
+Console.WriteLine("Digite a quantidade garçons:");
+qtd = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite a quantidade de horas do evento:");
+horas = Convert.ToInt32(Console.ReadLine());
+
+custo = Convert.ToDouble(qtd * horas * 10.55);
+
+Math.Round(custo, 2, MidpointRounding.ToEven);
+
+Console.WriteLine("O custo total com garçons para o evento " + nomeEvento + " é de R$" + custo + ".");
